16b7ac6ed8cb79a477b17d42a3ade6af252dd9ee
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-03-15 17:46:17 +1000

    Add functions to broken score

--- CP1404_practicals/prac_03/broken_score.py
+++ CP1404_practicals/prac_03/broken_score.py
@@ -3,12 +3,24 @@ CP1404/CP5632 - Practical - Suggested Solution
 Fixed program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or not score <= 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    """Get users score and print results"""
+    score = float(input("Enter score: "))
+    print(results(score))
+
+
+def results(score):
+    """obtain results from users score"""
+
+    if score < 0 or not score <= 100:
+        return 'invalid score'
+    elif score >= 90:
+        return 'Excellent'
+    elif score >= 50:
+        return 'Passable'
+    else:
+        return 'Bad'
+
+
+main()
