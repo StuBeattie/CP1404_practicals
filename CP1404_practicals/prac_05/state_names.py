1c30b100f2a75f7c8c2f7db9b24b9d79aa7f3272
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-03-24 13:36:49 +1000

    Added a loop to display state codes & names neatly in program

--- CP1404_practicals/prac_05/state_names.py
+++ CP1404_practicals/prac_05/state_names.py
@@ -7,7 +7,11 @@ File needs reformatting
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+
+# Display states code and names neatly lined up
+for code in CODE_TO_NAME:
+    print(code, "is", CODE_TO_NAME[code])
+print()
 
 # use while loop to iterate through CODE_TO_NAME and change any lower case inputs to upper case
 state_code = input("Enter short state: ").upper()
