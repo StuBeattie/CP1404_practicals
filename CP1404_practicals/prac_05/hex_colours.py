e4c4789b066db8bd41856a51d70cc005744c59a7
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-03-27 11:32:08 +1000

    Loop through dictionary and display users colour code

--- CP1404_practicals/prac_05/hex_colours.py
+++ CP1404_practicals/prac_05/hex_colours.py
@@ -1,14 +1,19 @@
 """Hexadecimal colour coder program"""
 
-COLOUR_CODE = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb",
-               "AntiqueWhite2": "#eedfcc", "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
-               "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6", "aquamarine4": "#458b74", "azure1": "#f0ffff"}
+COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb",
+                "AntiqueWhite2": "#eedfcc", "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
+                "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6", "aquamarine4": "#458b74", "azure1": "#f0ffff"}
 
 # print a list of colour names
-for name in COLOUR_CODE:
+for name in COLOUR_CODES:
     print(name)
 print()
 
-# ask the user to input a colour
+# ask the user to input a colour then display colour and code
 colour = input("Please enter a colour: ")
-
+while colour != "":
+    if colour in COLOUR_CODES:
+        print("The code for {} is: {}".format(colour, COLOUR_CODES[colour]))
+    else:
+        print("Invalid colour name!")
+    colour = input("Please enter a colour: ")
