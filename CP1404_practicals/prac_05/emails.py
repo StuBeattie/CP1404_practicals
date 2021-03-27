c8829de0d0e7bdd9693414a3c4dd94a1fd27beee
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-03-27 16:09:46 +1000

    store and display multiple name and email address

--- CP1404_practicals/prac_05/emails.py
+++ CP1404_practicals/prac_05/emails.py
@@ -1,20 +1,25 @@
 """Users email information"""
 
 email_information = {}
+# Get email address and split into parts
 user_email = input("Please enter your email: ")
-get_name_from_email = user_email.split("@")[0]
-split_email = get_name_from_email.split(".")
+while user_email != "":
+    get_name_from_email = user_email.split("@")[0]
+    split_email = get_name_from_email.split(".")
 
-# Split email and find users name
-name = "".join(split_email).title()
-print(" Is your name", name, "(Y/N)")
+    # Find users name in email address
+    name = "".join(split_email).title()
+    print(" Is your name", name, "(Y/N)")
 
-# confirm users name is correct. If not enter their real name
-confirm_name = input("-->: ").upper()
-if confirm_name != "Y" and confirm_name != "":
-    name = input("Please enter your name: ")
+    # confirm users name is correct. If not, enter their name
+    confirm_name = input("-->: ").upper()
+    if confirm_name != "Y" and confirm_name != "":
+        name = input("Please enter your name: ")
 
-# store name in directory( email_information) then display information neatly
-email_information[user_email] = name
-print("{} --> ({})".format(name, user_email))
+    # store name in directory(email_information) then display information neatly
+    email_information[user_email] = name
+    user_email = input("Please enter your email: ")
 
+# Display users names and email address neatly
+for user_email, name in email_information.items():
+    print("{} --> ({})".format(name, user_email))
