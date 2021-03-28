996496cdd1e9b9ceec978e971f0cf42862ae9200
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-03-28 15:57:13 +1000

    Implement a function to get user name from email address.

--- CP1404_practicals/prac_05/emails.py
+++ CP1404_practicals/prac_05/emails.py
@@ -1,25 +1,31 @@
 """Users email information"""
 
-email_information = {}
-# Get email address and split into parts
-user_email = input("Please enter your email: ")
-while user_email != "":
-    get_name_from_email = user_email.split("@")[0]
-    split_email = get_name_from_email.split(".")
 
-    # Find users name in email address
-    name = "".join(split_email).title()
-    print(" Is your name", name, "(Y/N)")
-
-    # confirm users name is correct. If not, enter their name
-    confirm_name = input("-->: ").upper()
-    if confirm_name != "Y" and confirm_name != "":
-        name = input("Please enter your name: ")
-
-    # store name in directory(email_information) then display information neatly
-    email_information[user_email] = name
+def main():
+    """Store email and names in dictionary."""
+    email_information = {}
     user_email = input("Please enter your email: ")
+    # Check name in users email is correct. if not enter their name.
+    while user_email != "":
+        name = get_name_from_email(user_email)
+        confirm_name = input(" Is your name {}? (Y/N): ".format(name)).upper()
+        if confirm_name != "Y" and confirm_name != "":
+            name = input("Please enter your name: ")
+        email_information[user_email] = name
+        user_email = input("Please enter your email: ")
 
-# Display users names and email address neatly
-for user_email, name in email_information.items():
-    print("{} --> ({})".format(name, user_email))
+        # Display users names and email address neatly
+    for user_email, name in email_information.items():
+        print("{} --> ({})".format(name, user_email))
+
+
+def get_name_from_email(user_email):
+    """Split email and get users name."""
+    split_email = user_email.split("@")[0]
+    split_email_at_decimal = split_email.split(".")
+    # Find users name in email address
+    name = " ".join(split_email_at_decimal).title()
+    return name
+
+
+main()
