60d700a4325e4388d6b0a728f34409d93ab132ac
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-07 20:48:57 +1000

    Add car object with string formatting to program.

--- CP1404_practicals/prac_06/car.py
+++ CP1404_practicals/prac_06/car.py
@@ -4,11 +4,13 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
+        name: string, name of the car
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
         self.odometer = 0
 
@@ -29,3 +31,7 @@ class Car:
             self.fuel -= distance
         self.odometer += distance
         return distance
+
+    def __str__(self):
+        """Display car object in string formatting."""
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel, self.odometer)
