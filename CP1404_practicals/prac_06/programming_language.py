26b3e45bdba7d2318c4dec748a3b4bb5cfc55d98
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-08 14:23:55 +1000

    Added string formatting to display information.

--- CP1404_practicals/prac_06/programming_language.py
+++ CP1404_practicals/prac_06/programming_language.py
@@ -4,10 +4,21 @@
 class ProgrammingLanguage:
     """Represent a programing language."""
 
-    def __init__(self, field="", typing="", reflection="", year=0):
+    def __init__(self, field, typing, reflection, year):
         """Initialise table instance."""
 
         self.field = field
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def is_dynamic(self):
+        """Confirm which language is dynamically typed."""
+
+        return self.typing == "Dynamic"
+
+    def __str__(self):
+        """Display programing language in string format."""
+
+        return "{}, {} Typing, Reflection={}," \
+               " First appeared in {}".format(self.field, self.typing, self.reflection, self.year)
