ab0b8d5269a786f0cebcb723344dfbded51656a9
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-09 22:15:54 +1000

    Add comments to program.

--- CP1404_practicals/prac_06/guitar_test.py
+++ CP1404_practicals/prac_06/guitar_test.py
@@ -10,14 +10,16 @@ def test():
     year = 1922
     cost = 16035.40
 
+    # Test to find out if the get_age function is displaying the correct information.
     guitar = GuitarInformation(name, year, cost)
     other = GuitarInformation("Another Guitar", 2013, 1999.99)
     print("{} get_age() - Expected {}. Got {}".format(guitar.name, 99, guitar.get_age()))
     print("{} get_age() - Expected {}. Got {}".format(guitar.name, 8, other.get_age()))
 
+    # Test to find out if the is_vintage function is displaying the correct information.
     print("{} is_vintage() - Expected {}. Got {}".format(guitar.name, "True", guitar.is_vintage()))
     print("{} is_vintage() - Expected {}. Got {}".format(guitar.name, "False", other.is_vintage()))
 
 
-if __name__ == "__main__":
+if __name__ != "__main__":
     test()
