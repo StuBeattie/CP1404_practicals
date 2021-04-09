44f75e377a9f493c1e386ba6c74367f188b02a2d
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-09 22:07:47 +1000

    Display all users guitars neatly.

--- CP1404_practicals/prac_06/guitars.py
+++ CP1404_practicals/prac_06/guitars.py
@@ -9,6 +9,7 @@ def main():
 
     guitars = []
 
+    # Get users to input guitar information and print to screen
     print("My guitars!")
     name = input("Name: ")
     while name != "":
@@ -20,9 +21,19 @@ def main():
         print()
         name = input("Name: ")
 
-        guitars.append(GuitarInformation("Gibson L-5 CES", 1922, 16035.40))
-        guitars.append(GuitarInformation("Line 6 JTV-59", 2010, 1512.9))
+    # Add extra guitars to guitars list.
+    guitars.append(GuitarInformation("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(GuitarInformation("Line 6 JTV-59", 2010, 1512.9))
+
+    # Display all users guitars neatly.
+    print()
+    print("These are my guitars:")
+    for i, guitar in enumerate(guitars):
+        vintage_string = ""
+        if guitar.is_vintage():
+            vintage_string = "(vintage)"
+        print("Guitar {}: {:>20} ({}), worth ${:10,.2f} {}"
+              .format(i + 1, guitar.name, guitar.year, guitar.cost, vintage_string))
 
 
-if __name__ == "__main__":
-    main()
+main()
