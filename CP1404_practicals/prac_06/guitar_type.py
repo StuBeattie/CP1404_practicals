555698e9a1ff7de41b688cb42cdfa7b32f28f036
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-09 15:03:18 +1000

    Add guitar information class with functions.

---
+++ CP1404_practicals/prac_06/guitar_type.py
@@ -0,0 +1,26 @@
+"""Store information on guitar types."""
+
+
+class GuitarInformation:
+    """Represent guitar information gathered from the user."""
+
+    def __init__(self, name="", year=0, cost=0):
+        """Initialise guitar instance."""
+
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def display_guitar_info(self):
+        """Display guitar information gathered from the user."""
+
+        return "{} ({}) : ${:.2f}".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        """Find the age of the guitar."""
+
+        return 2021 - self.year
+
+    def is_vintage(self):
+        """Identify if the guitar is >= 50 years old."""
+
