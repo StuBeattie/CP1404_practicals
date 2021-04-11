5758e673b26a789832cf2a3582b793a6f40c16a3
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-11 18:03:14 +1000

    Added ternary operator to improve readability.

--- CP1404_practicals/prac_06/guitars.py
+++ CP1404_practicals/prac_06/guitars.py
@@ -23,17 +23,15 @@ def main():
 
     # Add extra guitars to guitars list for testing.
     guitars.append(GuitarInformation("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(GuitarInformation("Line 6 JTV-59", 2010, 1512.9))
+    guitars.append(GuitarInformation("Line 6 JTV-59", 2010, 1512.90))
 
     # Display all users guitars neatly.
     print()
     print("These are my guitars:")
-    for i, guitar in enumerate(guitars):
-        vintage_string = ""
-        if guitar.is_vintage():
-            vintage_string = "(vintage)"
+    for i, guitar in enumerate(guitars, 1):
+        vintage_string = "(vintage)" if guitar.is_vintage() else ""
         print("Guitar {}: {:>20} ({}), worth ${:10,.2f} {}"
-              .format(i + 1, guitar.name, guitar.year, guitar.cost, vintage_string))
+              .format(i, guitar.name, guitar.year, guitar.cost, vintage_string))
 
 
 main()
