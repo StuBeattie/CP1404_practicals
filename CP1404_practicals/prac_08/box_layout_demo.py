a769c7f7525528f61e1d744efc2239e1eb774822
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-19 21:58:49 +1000

    Add text input to widget.

--- CP1404_practicals/prac_08/box_layout_demo.py
+++ CP1404_practicals/prac_08/box_layout_demo.py
@@ -15,7 +15,6 @@ class BoxLayoutDemo(App):
 
     def handle_greet(self):
         """Connect buttons with GUI to allow user interaction."""
-        print("Greet")
         self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
 
 
