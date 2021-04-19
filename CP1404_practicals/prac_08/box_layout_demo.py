abae167696513f1961bb0a9221e51df91080b0c1
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-19 21:43:51 +1000

    Added box layout demo program.

---
+++ CP1404_practicals/prac_08/box_layout_demo.py
@@ -0,0 +1,22 @@
+"""User greeting GUI"""
+
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    """Greet the user by name using a GUI."""
+
+    def build(self):
+        """Build box layout."""
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file('box_layout.kv')
+        return self.root
+
+    def handle_greet(self):
+        """Connect buttons with GUI to allow user interaction."""
+        print("Greet")
+        self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
+
+
+BoxLayoutDemo().run()
