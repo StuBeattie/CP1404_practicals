ff2304f186e3087a985fbd49df44628a4822cc66
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-20 10:22:37 +1000

    Added method to class for clearing text.

--- CP1404_practicals/prac_08/box_layout_demo.py
+++ CP1404_practicals/prac_08/box_layout_demo.py
@@ -17,5 +17,10 @@ class BoxLayoutDemo(App):
         """Connect buttons with GUI to allow user interaction."""
         self.root.ids.output_label.text = "Hello " + self.root.ids.input_name.text
 
+    def clear_text(self):
+        """Clear text from Label and TextInput."""
+        self.root.ids.output_label.text = ""
+        self.root.ids.input_name.text = ""
+
 
 BoxLayoutDemo().run()
