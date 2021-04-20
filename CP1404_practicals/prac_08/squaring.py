3e9c344adff188b18e4001331e42de68633111c3
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-20 12:45:35 +1000

    Introduce error handling for user input.

--- CP1404_practicals/prac_08/squaring.py
+++ CP1404_practicals/prac_08/squaring.py
@@ -22,8 +22,11 @@ class SquareNumberApp(App):
 
     def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
-        result = value ** 2
-        self.root.ids.output_label.text = str(result)
+        try:
+            result = float(value) ** 2
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            self.root.ids.output_label.text = 'Must be\n integer!'
 
 
 SquareNumberApp().run()
