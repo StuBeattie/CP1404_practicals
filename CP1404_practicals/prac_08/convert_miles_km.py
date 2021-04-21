f7971188d549ee1db1f898f24a5d6f78a5d9c2fe
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-21 21:43:43 +1000

    Produce correct layout of GUI.

--- CP1404_practicals/prac_08/convert_miles_km.py
+++ CP1404_practicals/prac_08/convert_miles_km.py
@@ -7,12 +7,26 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+__author__ = "Stuart Beattie"
 
-__author__="Stuart Beattie"
 
-class MilesToKilometersApp():
+class MilesToKilometersApp(App):
     """Convert miles to kilometers GUI with plus or minus integer adjustment buttons"""
-    pass
+
+    def build(self):
+        """ build the Kivy app from the kv file """
+        Window.size = (600, 300)
+        self.title = "Convert Miles to Kilometers"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
+
+    def handle_calculate(self, value):
+        """ handle calculation (could be button press or other call), output result to label widget """
+        try:
+            result = float(value) * 1.60934
+            self.root.ids.output_label.text = str("{:,.3f}".format(result))
+        except ValueError:
+            self.root.ids.output_label.text = 'Must be\n integer!'
 
 
 MilesToKilometersApp().run()
