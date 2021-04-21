cf16c4ad95bf082bba4e1aa36d67c4b9569da007
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-21 19:31:19 +1000

    Added convert miles to kilometers program.

---
+++ CP1404_practicals/prac_08/convert_miles_km.py
@@ -0,0 +1,18 @@
+"""
+Kivy GUI program to convert Miles to Kilometers.
+Stuart Beattie
+Started: 21/04/21
+"""
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+__author__="Stuart Beattie"
+
+class MilesToKilometersApp():
+    """Convert miles to kilometers GUI with plus or minus integer adjustment buttons"""
+    pass
+
+
+MilesToKilometersApp().run()
