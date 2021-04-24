fb97d2511762ad3b6684664913397fd363fec26a
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-24 18:44:58 +1000

    Added error checking method.

--- CP1404_practicals/prac_08/convert_miles_km.py
+++ CP1404_practicals/prac_08/convert_miles_km.py
@@ -24,30 +24,29 @@ class MilesToKilometersApp(App):
 
     def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
-        try:
-            result = float(value) * MILES_TO_KM_CONVERSION
-            self.root.ids.output_label.text = "{:,.3f}".format(result)
-        except ValueError:
-            # If error occurs reset input number to zero
-            self.root.ids.output_label.text = str(0.0)
+        result = self.sort_exceptions(value) * MILES_TO_KM_CONVERSION
+        self.root.ids.output_label.text = "{:,.3f}".format(result)
 
     def increase_increment(self, value):
         """Increase the input value by adding one then recalculate."""
-        try:
-            new_value = int(value) + INCREASE_VALUE
-            self.root.ids.input_number.text = str(new_value)
-            self.handle_calculate(new_value)
-        except ValueError:
-            self.root.ids.input_number.text = str(1)
+        new_value = int(self.sort_exceptions(value)) + INCREASE_VALUE
+        self.root.ids.input_number.text = str(new_value)
+        self.handle_calculate(new_value)
 
     def decrease_increment(self, value):
         """Decrease the input value by subtracting one then recalculate."""
+
+        new_value = int(self.sort_exceptions(value)) - INCREASE_VALUE
+        self.root.ids.input_number.text = str(new_value)
+        self.handle_calculate(new_value)
+
+    @staticmethod
+    def sort_exceptions(value):
+        """Conduct error checking on inputs and button presses."""
         try:
-            new_value = int(value) - INCREASE_VALUE
-            self.root.ids.input_number.text = str(new_value)
-            self.handle_calculate(new_value)
+            return float(value)
         except ValueError:
-            self.root.ids.input_number.text = str(-1)
+            return 0.0
 
 
 MilesToKilometersApp().run()
