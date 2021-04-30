ce9bf7e0428463d45a5f9751e81bd28fc7f471cb
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-04-30 20:15:17 +1000

    Add class variables for cleaner code.

--- CP1404_practicals/prac_08/taxi_test.py
+++ CP1404_practicals/prac_08/taxi_test.py
@@ -6,8 +6,10 @@ from prac_08.taxi import Taxi
 
 
 def main():
-    """ test taxi class"""
-    taxi_cab = Taxi("Prius 1", 100, 1.23)
+    """ test taxi class by displaying information."""
+    # enter name of taxi and units of fuel: Taxi("name", fuel)
+    taxi_cab = Taxi("Prius 1", 100)
+    # enter distance in km
     taxi_cab.drive(40)
     print(taxi_cab)
     taxi_cab.start_fare()
