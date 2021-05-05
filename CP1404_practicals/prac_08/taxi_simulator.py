3f61c81c1846fc5c168be79e46344d068420d117
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-05-05 21:28:30 +1000

    Connect menu with taxi classes.

--- CP1404_practicals/prac_08/taxi_simulator.py
+++ CP1404_practicals/prac_08/taxi_simulator.py
@@ -3,7 +3,6 @@ Create taxi simulator that utilizes
  Taxi and SilverServiceTaxi classes.
 """
 
-from prac_08.car import Car
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
@@ -13,8 +12,9 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     """Create a taxi simulator program that utilizes Taxi and SilverServiceTaxi classes.
     This program will loop through until the user quites."""
-
+    total_cost = 0
     taxi_types = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
 
     print("Lets drive!")
     print(MENU)
@@ -25,16 +25,34 @@ def main():
             print("Taxis available: ")
             taxis_available(taxi_types)
             taxi_choice = int(input("choose taxi: "))
-            taxi_chosen = taxi_types[taxi_choice]
-            taxis_available(taxi_types)
+            try:
+                current_taxi = taxi_types[taxi_choice]
+            except IndexError:
+                print("Invalid choice")
         elif menu_choice == "d":
-            print("lets drive taxi")
+            if current_taxi:
+                current_taxi.start_fare()
+                driving_distance = float(input("Drive how far? "))
+                current_taxi.drive(driving_distance)
+                cost = current_taxi.get_fare()
+                print("Your {} trip cost you ${:.2f}".format(current_taxi.name, cost))
+                total_cost += cost
+            else:
+                print("you need to choose a taxi before you can drive")
+        else:
+            print("Invalid option")
+        print("Bill to date: ${:.2f}".format(total_cost))
+        print(MENU)
+        menu_choice = input(">>> ").lower()
+    print("Bill to date: ${:.2f}".format(total_cost))
+    print("Taxis are now:")
+    taxis_available(taxi_types)
 
 
 def taxis_available(taxi_types):
     """Display taxis available with index for users to choose from"""
     for number, taxi in enumerate(taxi_types):
-        print("{} - {}".format(number + 1, taxi))
+        print("{} - {}".format(number, taxi))
 
 
 main()
