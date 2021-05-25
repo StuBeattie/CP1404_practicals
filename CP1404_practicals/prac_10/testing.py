58bfa99fcf9ef6f751b408548b833e0a445c3a62
StuBeattie <stuart.beattie1@my.jcu.edu.au>
2021-05-25 21:21:43 +1000

    complete first 4 TODO items in testing.py.

--- CP1404_practicals/prac_10/testing.py
+++ CP1404_practicals/prac_10/testing.py
@@ -22,7 +22,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -32,32 +32,30 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
-    # Hint: "-".join(["yo", "yo"] -> "yo-yo"
-
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
     test_car = Car(fuel=10)
+    assert test_car.fuel == 10, "Car does not set Fuel correctly"
+
+    test_car = Car()
+    assert test_car.fuel == 0, "Car does not set Fuel correctly"
 
 
-run_tests()
-
-# TODO: 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
-# doctest.testmod()
+doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
-# (don't change the tests, change the function!)
-
-# TODO: 5. Write and test a function to format a phrase as a sentence,
+def phrase_as_sentence(phrase):
+    """format a phrase as a sentence,
+    starting with a capital and ending with a single full stop.
+    >>> phrase_as_sentence(hello)
+    """
 # starting with a capital and ending with a single full stop.
 # Important: start with a function header and just use pass as the body
 # then add doctests for 3 tests:
@@ -66,3 +64,6 @@ run_tests()
 # and one more you decide (one that is valid!)
 # test this and watch the tests fail
 # then write the body of the function so that the tests pass
+
+
+run_tests()
