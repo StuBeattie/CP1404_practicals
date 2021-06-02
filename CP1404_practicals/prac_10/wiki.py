ed67261ac74789412ea120804198cdff7121ba71
StuBeattie <79081146+StuBeattie@users.noreply.github.com>
2021-06-02 16:20:51 +1000

    Update while loop to remove while true statement and Break statement.

--- CP1404_practicals/prac_10/wiki.py
+++ CP1404_practicals/prac_10/wiki.py
@@ -3,13 +3,10 @@
 import wikipedia
 
 # Get the user to input a title or phrase to search, then display results.
-while True:
+title_or_phrase = input("Enter title or phrase: ")
+while title_or_phrase != "":
     try:
-        title_or_phrase = input("Enter title or phrase: ")
-        if title_or_phrase == "":
-            break
-        else:
-            page = wikipedia.page(title_or_phrase)
-            print("{}\n {}\n {}\n".format(page.title, page.summary, page.url))
+        page = wikipedia.page(title_or_phrase)
+        print("{}\n {}\n {}\n".format(page.title, page.summary, page.url))
     except wikipedia.exceptions.DisambiguationError as e:
         print(e.options)
